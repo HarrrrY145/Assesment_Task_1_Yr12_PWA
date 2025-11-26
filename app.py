82157dc618c3ba92ca37bb3754a622e29cab7830
HarrrrY145 <harry.burton1@education.nsw.gov.au>
2025-11-26 21:38:35 +1100

    Creating a page to create a new acount by creating a new Login ID and a password which is then stored into a database.

--- app.py
+++ app.py
@@ -11,14 +11,47 @@ app.secret_key = os.urandom(24)
 def login():
     return render_template('login.html')
 
+@app.route('/login_validation', methods=['POST'])
+def login_valdiation():
+    login_ID = request.form.get('login_ID')
+    password = request.form.get('password')
 
-@app.route('/signUp')
+    connection = sqlite3.connect('LoginData.db')
+    cursor = connection.cursor()
+
+    user = cursor.execute("SELECT * FROM USERS WHERE login_ID=? AND password=?", (login_ID,password)).fetchall()
+    if len(user) > 0:
+        return render_template('userHome.html')
+    else:
+        return redirect('/')
+
+@app.route('/verification')
 def signUp():
-    return render_template('signUp.html')
+    return render_template('verification.html')
 
 @app.route('/newUser')
 def newUser():
     return render_template('newUser.html')
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    Login_ID = request.form.get('Login_ID')
+    password =request.form.get('password')
+    
+    connection = sqlite3.connect('LoginData.db')
+    cursor = connection.cursor()
+
+    ans = cursor.execute("select * from USERS where Login_ID=? AND password=?",(Login_ID,password)).fetchall()
+
+    if len(ans) > 0:
+        connection.close()
+        return render_template('login.html')
+    else:
+        cursor.execute("INSERT INTO USERS(Login_ID,password)values(?,?)",(Login_ID,password))
+        connection.commit()
+        connection.close()
+        return render_template('login.html')
+
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=8080)
